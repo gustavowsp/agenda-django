a1b56d7e42c7607445ec0eb1649e647380cd0b02
Gustavo <Gustavocampos50@proton.me>
2023-07-23 13:49:00 -0300

    Registrando o model Contact-app:contact na área administrativa do django

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from contact import models
+
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id','primeiro_nome','ultimo_nome','data_criacao']
+    list_display_links = ['id','primeiro_nome']
+    ordering = ['-id']
+    search_fields = ['primeiro_nome','ultimo_nome']
+    list_filter = ['data_criacao','id']
+    #list_editable = ['ultimo_nome']
+    list_per_page = 1
+    list_max_show_all = 10
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+  primeiro_nome = models.CharField(max_length=255)
+  ultimo_nome   = models.CharField(max_length=255)
+  telefone      = models.CharField(max_length=255)
+  email         = models.EmailField(max_length=255,blank=True)
+  data_criacao  = models.DateTimeField(default=timezone.now)
+  descricao     = models.TextField(blank=True)
+
+  def __str__(self) -> str:
+    return f'{self.primeiro_nome} {self.ultimo_nome}'
